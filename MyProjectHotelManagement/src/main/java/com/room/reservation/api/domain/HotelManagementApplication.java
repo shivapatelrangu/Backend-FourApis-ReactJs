bbc7fd8d009dc1b544e52055558aef5d0ec0eef2
Shiva <2262923@cognizant.com>
2024-07-24 14:32:03 +0530

    Commiting frontend and backend

---
+++ MyProjectHotelManagement/src/main/java/com/room/reservation/api/domain/HotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.room.reservation.api.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementApplication.class, args);
+	}
+
+}
---
+++ MyProjectHotelManagement/src/test/java/com/room/reservation/api/domain/HotelManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.room.reservation.api.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads()
+	{
+		
+	}
+}
